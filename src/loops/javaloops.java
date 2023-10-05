f8379b1fac52a7580ebffc255d492b4814288778
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2023-10-05 13:28:16 -0700

    control structures assignment 1

---
+++ src/loops/javaloops.java
@@ -0,0 +1,135 @@
+package loops;
+import java.util.Scanner;
+public class javaloops {
+
+	public static void main(String[] args) {
+		int num;
+		
+		System.out.println("0:Sh20(30mins ,3hrs");
+		System.out.println("1:Sh50(40mins+50SMS,24hrs");
+		System.out.println("2:Okoa Easy Sh500(500Mins ,7days");
+		System.out.println("3:Okoa 500");
+		System.out.println("4:Okoa 250");
+		System.out.println("5:Okoa 100");
+		System.out.println("6:Okoa 50");
+		System.out.println("7:Okoa 20");
+		System.out.println("8:Okoa 10");
+		System.out.println("98:More");
+		System.out.println("Enter option:");
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		
+		switch (num) {
+		case 0:
+			int case1;
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:20");
+			System.out.println("TOTAL DEBT will be:20");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("0:BACK 00:HOME");
+			
+			Scanner S2=new Scanner(System.in);
+		case1=sc.nextInt();
+		switch (case1) {
+		case 1:
+			System.out.println("Your okoa has been accepted");
+			break;
+		case 2:
+			System.out.println("Your okoa has been succesfully cancelled");
+			break;
+			default:
+				System.out.println("0:Sh20(30mins ,3hrs");
+				System.out.println("1:Sh50(40mins+50SMS,24hrs");
+				System.out.println("2:Okoa Easy Sh500(500Mins ,7days");
+				System.out.println("3:Okoa 500");
+				System.out.println("4:Okoa 250");
+				System.out.println("5:Okoa 100");
+				System.out.println("6:Okoa 50");
+				System.out.println("7:Okoa 20");
+				System.out.println("8:Okoa 10");
+				System.out.println("98:More");
+				System.out.println("Enter option:");
+			
+		}
+			
+			break;
+			
+		case 1:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:0");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("Existing unpaid Okoa:0");
+			break;
+		case 2:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:0");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("Existing unpaid Okoa:0");
+			break;
+		case 3:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:0");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("Existing unpaid Okoa:0");
+			break;
+		case 4:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:0");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("Existing unpaid Okoa:0");
+			break;
+		case 5:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:0");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("Existing unpaid Okoa:0");
+			break;
+		case 6:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:0");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("Existing unpaid Okoa:0");
+			break;
+		case 7:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:0");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("Existing unpaid Okoa:0");
+			break;
+		case 8:
+			System.out.println("Existing unpaid Okoa:0");
+			System.out.println("New Okoa request:0");
+			System.out.println("TOTAL DEBT will be:50");
+			System.out.println("1:Accept");
+			System.out.println("2:Decline");
+			System.out.println("Existing unpaid Okoa:0");
+			break;
+			default:
+				System.out.println("do you need assistance");
+			
+			
+			
+			
+			
+			
+			
+			
+		
+	}
+
+	}}
